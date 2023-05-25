dc24b47cd5227b6c95b81619494263cb4bf1af91
KaioMarxDev <kaiomarxdev@gmail.com>
2023-05-25 09:11:06 -0300

    add: python 1768 problem solve

---
+++ 1000-2000 LeetCode/1768.py
@@ -0,0 +1,22 @@
+def merge_strings(word1, word2) -> str:
+  len1 = len(word1)
+  len2 = len(word2)
+  merged = []
+  i=0
+  while i < len1 and i < len2:
+    merged.append(word1[i])
+    merged.append(word2[i])
+    i += 1
+  
+  if i < len1:
+    merged.extend(word1[i:])
+  elif i < len2:
+    merged.extend(word2[i:])
+
+  return ''.join(merged)
+def main():
+  print(merge_strings("abc", "def"))
+  print(merge_strings("abc", "defgh"))
+
+if __name__ == "__main__":
+  main()
