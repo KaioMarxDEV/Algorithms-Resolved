917cee49a59c46a8f82483b896aa8b85cc77d765
KaioMarxDev <kaiomarxdev@gmail.com>
2023-05-29 12:26:00 -0300

    add: solution of leet code 01

---
+++ 0-100 LeetCode/1.py
@@ -0,0 +1,9 @@
+def twoSum(self, nums: list[int], target: int) -> list[int]:
+    hashmap = {}
+    i = 0
+    while i < len(nums):
+        diff = target - nums[i]
+        if diff in hashmap:
+            return [hashmap[diff], i]
+        hashmap[diff] = i
+        i += 1
